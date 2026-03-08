301d32bfe3c99c89ea2eee170ab4bcbf742b2031
Sundaye <2571511554@qq.com>
2026-03-08 23:51:43 +0800

    第四章

---
+++ P4_RAG项目实例/app_file_uploader.py
@@ -0,0 +1,39 @@
+"""
+基于Streamlit完成WEB网页上传服务
+
+当WEB页面元素发生变化，则整个程序代码重新执行一遍
+"""
+
+import streamlit as st
+from knowledge_base import KnowledgeBaseService
+
+# 添加网页标题
+st.title("知识库更新服务")
+
+# file_uploader
+uploader_file = st.file_uploader(
+    "请上传TXT文件",
+    type=['txt'],
+    accept_multiple_files=False,    # 仅接受一个文件
+)
+
+# session_state就是一个字典
+if "service" not in st.session_state:       # 维护上传次数
+    st.session_state["service"] = KnowledgeBaseService()
+
+
+if uploader_file is not None:
+    # 提取文件信息
+    file_name = uploader_file.name
+    file_type = uploader_file.type
+    file_size = uploader_file.size / 1024   # KB
+
+    st.subheader(f"文件名：{file_name}")
+    st.write(f"格式：{file_type} | 大小：{file_size:.2f} KB")
+
+    # get_value获取文件内容 -> bytes -> decode('utf-8')
+    text = uploader_file.getvalue().decode("utf-8")
+
+    result = st.session_state["service"].upload_by_str(text, file_name)
+    st.write(result)
+
