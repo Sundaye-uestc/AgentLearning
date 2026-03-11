04ef44ec47efb5ff9997b7378dfa9d1599e818c9
Sundaye <2571511554@qq.com>
2026-03-11 16:38:22 +0800

    prompt功能

---
+++ Agent项目/utils/config_handler.py
@@ -0,0 +1,38 @@
+"""
+yaml
+k: v
+"""
+import os
+
+import yaml
+
+from Agent项目.utils.path_tool import get_abs_path
+
+project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def load_rag_config(config_path: str=get_abs_path(project_root_path+"/config/rag.yaml"), encoding: str="utf-8"):
+    with open(config_path, "r", encoding=encoding) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def load_chroma_config(config_path: str=get_abs_path(project_root_path+"/config/chroma.yaml"), encoding: str="utf-8"):
+    with open(config_path, "r", encoding=encoding) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def load_agent_config(config_path: str=get_abs_path(project_root_path+"/config/agent.yaml"), encoding: str="utf-8"):
+    with open(config_path, "r", encoding=encoding) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def load_prompts_config(config_path: str=get_abs_path(project_root_path+"/config/prompts.yaml"), encoding: str="utf-8"):
+    with open(config_path, "r", encoding=encoding) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+rag_conf = load_rag_config()
+chroma_conf = load_chroma_config()
+agent_conf = load_agent_config()
+prompts_conf = load_prompts_config()
+
+if __name__ == '__main__':
+    print(rag_conf["chat_model_name"])
