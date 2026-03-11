3aaa3a13d589f516a440416918e9b9afffbb1610
Sundaye <2571511554@qq.com>
2026-03-11 22:13:14 +0800

    agent工具开发

---
+++ Agent项目/agent/tools/agent_tools.py
@@ -0,0 +1,86 @@
+import os
+import random
+
+from langchain_core.tools import tool
+from Agent项目.rag.rag_service import RagSummarizeService
+from Agent项目.utils.config_handler import agent_conf
+from Agent项目.utils.logger_handler import logger
+from Agent项目.utils.path_tool import get_abs_path
+
+rag = RagSummarizeService()
+external_data = {}
+
+@tool(description="从向量存储库中检索参考资料")
+def rag_summarize(query: str) -> str:
+    return rag.rag_summarize(query)
+
+@tool(description="获取城市天气，以消息字符串形式返回")
+def get_weather(city: str) -> str:
+    return f"城市{city}天气为晴天，气温26℃，空气湿度50%，南风1级，AQI21，最近6小时降雨概率极低"
+
+@tool(description="获取用户所在城市名称，以str返回")
+def get_user_city() -> str:
+    return random.choice(["深圳", "成都", "上海"])
+
+@tool(description="获取用户ID，以str返回")
+def get_user_id() -> str:
+    return random.choice(["1001","1002","1003","1004","1005","1006","1007","1008","1009","1010"])
+
+@tool(description="获取当前月份，以str形式返回")
+def get_month() -> str:
+    return random.choice(["2025-01","2025-02","2025-03","2025-04","2025-05","2025-06","2025-07","2025-08","2025-09","2025-10","2025-11","2025-12"])
+
+
+def generate_external_data():
+    """
+    {
+        "user_id": {
+            "month" : {"特征"：XXX，"效率"：XXX，...}
+            "month" : {"特征"：XXX，"效率"：XXX，...}
+            "month" : {"特征"：XXX，"效率"：XXX，...}
+            ...
+        }
+    }
+    :return:
+    """
+    if not external_data:
+        external_data_path = get_abs_path(agent_conf["external_data_path"])
+
+        if not os.path.exists(external_data_path):
+            raise FileNotFoundError(f"外部数据文件{external_data_path}不存在")
+
+        with open(external_data_path, "r", encoding="utf-8") as f:
+            for line in f.readlines()[1:]:
+                arr: list[str] = line.strip().split(",")
+
+                user_id: str = arr[0].replace('"',"")
+                feature: str = arr[1].replace('"',"")
+                efficiency: str = arr[2].replace('"',"")
+                consumable: str = arr[3].replace('"',"")
+                comparison: str = arr[4].replace('"',"")
+                time: str = arr[5].replace('"',"")
+
+                if user_id not in external_data:
+                    external_data[user_id] = {}
+
+                external_data[user_id][time] = {
+                    "特征": feature,
+                    "效率": efficiency,
+                    "耗材": consumable,
+                    "对比": comparison,
+                }
+
+
+@tool(description="从外部系统中获取用户使用记录，以str返回，如果未检索到返回空字符串")
+def fetch_external_data(user_id: str, month: str) -> str:
+    generate_external_data()
+
+    try:
+        return external_data[user_id][month]
+    except KeyError:
+        logger.warning(f"[fetch_external_data]未能检索到用户：{user_id}在{month}的使用记录数据")
+        return ""
+
+
+# if __name__ == '__main__':
+#     print(fetch_external_data("1001", "2025-01"))
--- Agent项目/utils/path_tool.py
+++ Agent项目/utils/path_tool.py
@@ -2,20 +2,22 @@
 为整个工程提供统一的绝对路径
 """
 import os
+from pathlib import Path
 
 def get_project_root() -> str:
     """
     获取工程所在的根目录
     :return: 字符串根目录
     """
-    # 当前文件的绝对路径
-    current_file = os.path.abspath(__file__)
-    # 获取工程的根目录，先获取文件所在的文件夹绝对路径
-    current_dir = os.path.dirname(current_file)
-    # 获取工程根目录
-    project_root = os.path.abspath(current_dir)
-
-    return project_root
+    # # 当前文件的绝对路径
+    # current_file = os.path.abspath(__file__)
+    # # 获取工程的根目录，先获取文件所在的文件夹绝对路径
+    # current_dir = os.path.dirname(current_file)
+    # # 获取工程根目录
+    # project_root = os.path.abspath(current_dir)
+    #
+    # return project_root
+    return str(Path(__file__).resolve().parent.parent)
 
 def get_abs_path(relative_path: str) -> str:
     """
